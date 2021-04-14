f4810cb741ddff70ffd39bf7159d890f39b7a6cb
Matvey Ilichev <matvey.ilichev@gmail.com>
2021-04-14 19:29:06 +0500

    added exceptions new check_errors

--- form.py
+++ form.py
@@ -1,34 +1,50 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLineEdit, QLabel
 import dictionary
 import sys
+import EXCEPTIONS
+
+try:
+    from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel
+except Exception as e:
+    print('PyQt5 not found: "{}".'.format(e))
+    sys.exit(EXCEPTIONS.ERROR_QT_VERSION)
 
 
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
+        self.level_text = iter(dictionary.sentences['easy'])
+        self.set_user_interface()
+        self._errors = []
+
+    def set_user_interface(self):
         self.setWindowTitle('Keyboard simulator')
         self.setGeometry(500, 400, 600, 300)
+
         self.text_to_write = QLabel(self)
-        self.level_text = iter(dictionary.sentences['easy'])
-        self.text_to_write.setText(next(self.level_text))
         self.text_to_write.setGeometry(50, 100, 500, 40)
-        self.textbox = QLineEdit(self)
-        self.textbox.setGeometry(50, 200, 500, 40)
-        self.textbox.returnPressed.connect(self.on_click)
+        self.text_to_write.setText(next(self.level_text))
 
-    def on_click(self):
-        if(self.textbox.text() == self.text_to_write.text()):
-            print('OK!')
+        self.user_text_box = QLineEdit(self)
+        self.user_text_box.setGeometry(50, 200, 500, 40)
+        self.user_text_box.returnPressed.connect(self.on_click_enter)
+        self.user_text_box.textEdited.connect(self.check_errors)
+
+    def on_click_enter(self):
+        if self.user_text_box.text() == self.text_to_write.text():
+            print('correct sentence:)')
+            self.user_text_box.setText('')
+            try:
+                self.text_to_write.setText(next(self.level_text))
+            except StopIteration:
+                print('end of sentences')
         else:
-            print('Problem!')
-        self.textbox.setText('')
-        try:
-            self.text_to_write.setText(next(self.level_text))
-        except:
-            StopIteration
+            print('wrong sentence:(')
 
-
-
+    def check_errors(self):
+        self._errors = list(i for (i, (a, b)) in
+                            enumerate(zip(self.text_to_write.text(),
+                                          self.user_text_box.text()))
+                            if a != b)
 
 
 app = QApplication(sys.argv)
