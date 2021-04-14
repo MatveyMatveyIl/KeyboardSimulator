f059113e6300eb2b38843be54e092592c682cbd2
Matvey Ilichev <matvey.ilichev@gmail.com>
2021-04-14 23:23:12 +0500

    stat file

---
+++ statistic.py
@@ -0,0 +1,59 @@
+class Statistic:
+    def __init__(self):
+        self.statistic = {
+            'WPM': WPM(),
+            'claer_WPM': ClearWPM(),
+            'CPM': CPM(),
+            'clear_CPM': ClearCPM(),
+            'KPM': KPM(),
+            'clear_KPM': ClearKPM(),
+        }
+
+
+class WPM:
+    def __init__(self):
+        pass
+
+    def count_stat(self):
+        pass
+
+
+class ClearWPM:
+    def __init__(self):
+        pass
+
+    def count_stat(self):
+        pass
+
+
+class CPM:
+    def __init__(self):
+        pass
+
+    def count_stat(self):
+        pass
+
+
+class ClearCPM:
+    def __init__(self):
+        pass
+
+    def count_stat(self):
+        pass
+
+
+class KPM:
+    def __init__(self):
+        pass
+
+    def count_stat(self):
+        pass
+
+
+class ClearKPM:
+    def __init__(self):
+        pass
+
+    def count_stat(self):
+        pass
+
