af76e0679295703b1a1985057c1413526c21d391
Matvey Ilichev <matvey.ilichev@gmail.com>
2021-04-15 23:39:20 +0500

    timer in form

--- form.py
+++ form.py
@@ -2,10 +2,13 @@ import dictionary
 import sys
 import EXCEPTIONS
 import form_style
+import statistic
+from timer import *
 
 try:
     from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel
-    from PyQt5.QtGui import QIcon
+    from PyQt5.QtGui import QIcon, QTextCharFormat, QFont, QSyntaxHighlighter
+    from PyQt5.QtCore import QTimer
 except Exception as e:
     print('PyQt5 not found: "{}".'.format(e))
     sys.exit(EXCEPTIONS.ERROR_QT_VERSION)
@@ -17,11 +20,17 @@ class Window(QMainWindow):
         self.level_text = iter(dictionary.sentences['easy'])
         self.set_user_interface()
         self._errors = []
+        self.stat = statistic.Statistic()
+        self.timer = StopWatch()
 
     def set_user_interface(self):
         self.setWindowTitle('Keyboard simulator')
         self.setGeometry(500, 400, 600, 300)
         self.setWindowIcon(QIcon('pictures/programmIcon.png'))
+
+        self.timer_label = QLabel(self)
+        self.timer_label.setText('0:00.00')
+        self.timer_label.setGeometry(400, 50, 100, 100)
 
         self.text_to_write = QLabel(self)
         self.text_to_write.setGeometry(50, 100, 500, 40)
@@ -30,28 +39,46 @@ class Window(QMainWindow):
         self.user_text_box = QLineEdit(self)
         self.user_text_box.setGeometry(50, 200, 500, 40)
         self.user_text_box.returnPressed.connect(self.on_click_enter)
-        self.user_text_box.textEdited.connect(self.check_errors)
-        self.user_text_box.textEdited.connect(self.set_color)
+        self.user_text_box.textChanged.connect(self.check_errors)
+        self.user_text_box.textChanged.connect(self.update_time)#спросить
 
     def on_click_enter(self):
         if self.user_text_box.text() == self.text_to_write.text():
-            print('correct sentence:)')
             self.user_text_box.setText('')
             try:
+                self.timer.do_pause()
+                self.timer_label.setText('0:00.00')
                 self.text_to_write.setText(next(self.level_text))
+                print('correct')
             except StopIteration:
-                print('end of sentences')
-        else:
-            print('wrong sentence:(')
+                print('wrong')
+                pass #message
 
     def check_errors(self):
         self._errors = list(i for (i, (a, b)) in
                             enumerate(zip(self.text_to_write.text(),
                                           self.user_text_box.text()))
                             if a != b)
+        #print(self._errors)
 
-    def set_color(self):
-        self.text_to_write.setStyleSheet('background-color: #a6f5c8')
+    def update_time(self):
+        self.timer.do_start()
+        self.timer_label.setText(
+            "%d:%05.2f" % (self.timer.time // 60, self.timer.time % 60))
+
+
+    #def set_color(self):
+        #self.text_to_write.setStyleSheet('background-color: #a6f5c8;')
+        # if len(self._errors) == 0:
+        #     pass
+        # self.text_to_write.setStyleSheet('background-color: #ff6e6e;')
+
+# class SyntaxHighlighter(QSyntaxHighlighter):
+#     def __init__(self, parrent):
+#         super().__init__(parrent)
+#
+#     def color_line(self, line_num):
+#         self.rehighlightBlock()
 
 
 app = QApplication(sys.argv)
--- timer.py
+++ timer.py
@@ -1,51 +1,25 @@
-import sys
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel, QPushButton
-from PyQt5.QtGui import QIcon, QTextCharFormat, QFont, QSyntaxHighlighter
 from PyQt5.QtCore import QTimer
 
-TICK_TIME = 1
-
-class StopWatch(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setGeometry(500, 400, 600, 300)
+class StopWatch():
+    def __init__(self):
+        super().__init__()
+        self.time = 0
         self.timer = QTimer()
-        self.timer.setInterval(TICK_TIME)
+        self.timer.setInterval(1)
         self.timer.timeout.connect(self.tick)
-        self.time_text = QLabel(self)
-        self.time_text.setGeometry(100, 100, 100, 100)
-        self.start = QPushButton(self)
-        self.start.clicked.connect(self.do_start)
+        self.in_progress = False
 
-        self.do_reset()
-
-    def display(self):
-        self.time_text.setText("%d:%05.2f" % (self.time // 60, self.time % 60))
 
     def tick(self):
-        self.time += TICK_TIME/100
-        self.display()
+        self.time += 1/100
 
     def do_start(self):
-        self.timer.start(10)
-        self.start.setText("Pause")
-        self.start.clicked.disconnect()
-        self.start.clicked.connect(self.do_pause)
+        if not self.in_progress:
+            self.timer.start(10)
+            self.in_progress = True
 
     def do_pause(self):
         self.timer.stop()
-        self.start.setText("Start")
-        self.start.clicked.disconnect()
-        self.start.clicked.connect(self.do_start)
-
-    def do_reset(self):
         self.time = 0
-        self.display()
+        self.in_progress = False
 
-app = QApplication(sys.argv)
-
-watch = StopWatch()
-watch.show()
-
-sys.exit(app.exec_())
