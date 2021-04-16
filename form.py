5b59207df4a28076c0c68fcbcb039496150a5370
Matvey Ilichev <matvey.ilichev@gmail.com>
2021-04-16 20:40:26 +0500

    new interface

--- form.py
+++ form.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'p.ui'
 import dictionary
 import sys
 import EXCEPTIONS
@@ -6,7 +9,8 @@ import statistic
 from timer import *
 
 try:
-    from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel
+    from PyQt5.QtWidgets import QApplication, QMainWindow, \
+        QLineEdit, QLabel, QComboBox, QMenuBar, QMenu, QAction
     from PyQt5.QtGui import QIcon, QTextCharFormat, QFont, QSyntaxHighlighter
     from PyQt5.QtCore import QTimer
 except Exception as e:
@@ -24,24 +28,58 @@ class Window(QMainWindow):
         self.stop_watch = StopWatch()
 
     def set_user_interface(self):
+        self.set_window_interface()
+        self.set_user_text_box_interface()
+        self.set_text_to_write_interface()
+        self.set_level_box_interface()
+        self.set_stopwatch_interface()
+        self.set_menubar_interface()
+
+    def set_window_interface(self):
         self.setWindowTitle('Keyboard simulator')
-        self.setGeometry(500, 400, 600, 300)
+        self.setFixedSize(1028, 731)
         self.setWindowIcon(QIcon('pictures/programmIcon.png'))
 
-        self.timer_label = QLabel(self)
-        self.timer_label.setText('0:00.00')
-        self.timer_label.setGeometry(400, 50, 100, 100)
-
-        self.text_to_write = QLabel(self)
-        self.text_to_write.setGeometry(50, 100, 500, 40)
-        self.text_to_write.setText(next(self.level_text))
-
+    def set_user_text_box_interface(self):
         self.user_text_box = QLineEdit(self)
-        self.user_text_box.setGeometry(50, 200, 500, 40)
+        self.user_text_box.setGeometry(50, 360, 701, 291)
         self.user_text_box.returnPressed.connect(self.on_click_enter)
         self.user_text_box.textChanged.connect(self.check_errors)
         self.user_text_box.textChanged.connect(self.update_time)
-        #self.user_text_box.textChanged.connect(self.set_color)#спросить
+
+    def set_text_to_write_interface(self):
+        self.text_to_write = QLabel(self)
+        self.text_to_write.setGeometry(60, 90, 661, 181)
+        self.text_to_write.setText(next(self.level_text))
+
+    def set_level_box_interface(self):
+        self.level_box = QComboBox(self)
+        self.level_box.setGeometry(780, 100, 200, 60)
+        self.level_box.addItem("Уровень 1 - Слова")
+        self.level_box.addItem("Уровень 2 - Предложения")
+        self.level_box.addItem("Уровень 3 - Текст")
+        # self.level_box.activated.connect(self.set_color)
+
+    def set_stopwatch_interface(self):
+        self.timer_label = QLabel(self)
+        self.timer_label.setText('0:00.00')
+        self.timer_label.setGeometry(790, 360, 171, 81)
+
+    def set_menubar_interface(self):
+        self.statusBar()
+        menubar = self.menuBar()
+        menubar.setGeometry(0, 0, 1028, 26)
+        menu = menubar.addMenu('Меню')
+        stats = menubar.addMenu('Статистика')
+        help = menubar.addMenu('Помощь')
+        # self.stat1 = QAction("&По уровням", self)
+        self.stat2 = QAction("Лучший результат", self)
+        # stats.addAction(self.stat1)
+        stats.addAction(self.stat2)
+        levels = stats.addMenu("По уровням")
+        levels.addAction("Слова")
+        levels.addAction("Предложения")
+        levels.addAction("Текст")
 
     def on_click_enter(self):
         if self.user_text_box.text() == self.text_to_write.text():
@@ -68,7 +106,8 @@ class Window(QMainWindow):
 
     def print_time(self):
         self.timer_label.setText(
-            "%d:%05.2f" % (self.stop_watch.time // 60, self.stop_watch.time % 60))
+            "%d:%05.2f" % (self.stop_watch.time // 60,
+                           self.stop_watch.time % 60))
 
     def set_color(self):
         self.text_to_write.setStyleSheet('background-color: #a6f5c8;')
